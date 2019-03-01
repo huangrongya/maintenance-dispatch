8a78ccf07296178aab4949819d8d8c9281c2950f
huangrongya <huangrongya@foxmail.com>
2019-03-01 18:26:51 +0800

    注册到eureka注册中心

--- src/main/java/com/tepth/maintenancedispatch/MaintenanceDispatchApplication.java
+++ src/main/java/com/tepth/maintenancedispatch/MaintenanceDispatchApplication.java
@@ -3,9 +3,11 @@ package com.tepth.maintenancedispatch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-//@ServletComponentScan
+@EnableEurekaClient
 public class MaintenanceDispatchApplication {
 
     public static void main(String[] args) {
